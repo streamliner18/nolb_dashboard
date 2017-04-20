11b4ad86088043edae188b83cb2ae61267326f9e
Streamliner <jimmy.xrail@me.com>
2017-04-20 04:48:06 +0000

    Basic control

--- car.py
+++ car.py
@@ -1,4 +1,28 @@
 import requests as q
+from requests import Session, get
+from simplejson import loads
 
 class Car():
-	def __init__():
+	def __init__(self):
+		self.connected = False
+	def connect(self,ip_addr='', really=False):
+		if really:
+			self.session = Session()
+			r = self.session.get('http://'+ip_addr+'/status')
+			assert r.status_code == 200
+			self.connected = True
+			self.ip_addr = 'http://'+ip_addr+'/'
+		else:
+			self.session = None
+			self.connected = False
+	def exec_function(self, action, **kwargs):
+		assert self.connected
+		connection_string = self.ip_addr+action
+		if len(kwargs) > 0:
+			connection_string += '?' + ','.join([k+'='+v for k,v in kwargs.items()])
+		r = self.session.get(connection_string);
+		assert r.status_code == 200
+		return loads(r.content)
+		
+			
+car = Car()
--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,34 @@
 import ui
+import console
+
+from car import car
+
+
+@ui.in_background
+def sw_connect_action(sender):
+	global_activity.start()
+	try:
+		car.connect(ip_addr=sender.superview['txt_ip_addr'].text,really=sender.value)
+		console.hud_alert('Connection '+('Established' if sender.value else 'Ended'))
+	except Exception as e:
+		console.hud_alert('Could not '+('establish ' if sender.value else 'terminate ')+'connection: '+str(e))
+		sender.value = not sender.value
+	global_activity.stop()
+
+def sw_turn_action(sender):
+	global_activity.start()
+	try:
+		car.exec_function('heading/set-relative', value=sender.angle)
+	except Exception as e:
+		console.hud_alert(str(e))
+	global_activity.stop()
 
 v = ui.load_view()
-v.present('sheet')
+global_activity = ui.ActivityIndicator()
+global_activity.hides_when_stopped = True
+global_activity.style = ui.ACTIVITY_INDICATOR_STYLE_GRAY
+global_activity.center = v.width - 90, 80
+v.add_subview(global_activity)
+v.present('full_screen', orientations=["landscape-right"])
 
 
