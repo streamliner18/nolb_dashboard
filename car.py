e2c942fea99c85eac086ffc92de8de91ab59ccf0
Streamliner <jimmy.xrail@me.com>
2017-04-21 03:39:05 +0000

    Status Update

--- car.py
+++ car.py
@@ -6,6 +6,9 @@ class Car():
 	def __init__(self):
 		self.connected = False
 		self.executing = False
+		self.speed = 0
+		self.steps = 0
+		self.obstacle = 0
 	def connect(self,ip_addr='', really=False):
 		if really and (not self.executing):
 			self.session = Session()
@@ -13,11 +16,17 @@ class Car():
 			r = self.session.get('http://'+ip_addr+'/status')
 			self.executing = False
 			assert r.status_code == 200
+			try:
+				data = loads(r.content)
+				self.steps = data['steps']
+				self.obstacle	= data['obstacle']
+			except: pass
 			self.connected = True
 			self.ip_addr = 'http://'+ip_addr+'/'
 		else:
 			self.session = None
 			self.connected = False
+			
 	def exec_function(self, action, **kwargs):
 		assert self.connected and not self.executing
 		connection_string = self.ip_addr+action
@@ -32,6 +41,15 @@ class Car():
 			raise(e)
 		assert r.status_code == 200
 		return loads(r.content)
+
+	def reload_status(self, nc=False):
+		action = 'status-nc' if nc else 'status'
+		data = self.exec_function(action)
+		self.steps = data['steps-traversed']
+		self.obstacle	= data['obstacle']
 		
+	def reload_speed(self):
+		data = self.exec_function('speed')
+		self.speed = data['speed']
 			
 car = Car()
--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,16 @@ from car import car
 def log_item(view, item):
     view['txt_log'].text += '\n' + str(item)
 
+def redraw_status(v):
+	v['lbl_speed'].text = str(car.speed)
+	v['lbl_steps'].text = str(car.steps)
+	if car.obstacle == 0:
+		v['img_tcas_normal'].alpha = 1
+		v['img_tcas_triggered'].alpha = 0
+	else:
+		v['img_tcas_normal'].alpha = 0
+		v['img_tcas_triggered'].alpha = 1
+
 @ui.in_background
 def sw_connect_action(sender):
 	global_activity.start()
@@ -14,6 +24,7 @@ def sw_connect_action(sender):
 		log_item(sender.superview, 'Connecting')
 		car.connect(ip_addr=sender.superview['txt_ip_addr'].text,really=sender.value)
 		console.hud_alert('Connection '+('Established' if sender.value else 'Ended'), 'success',0.25)
+		redraw_status(sender.superview)
 		log_item(sender.superview, 'Connected' if sender.value else 'Disconnected')
 	except Exception as e:
 		console.hud_alert('Could not '+('establish ' if sender.value else 'terminate ')+'connection: '+str(e),'error',0.25)
@@ -31,33 +42,50 @@ def sw_turn_action(sender):
 		log_item(sender.superview, r)
 	except Exception as e:
 		console.hud_alert(str(e),'error',0.25)
+	redraw_status(sender.superview)
 	global_activity.stop()
 
 @ui.in_background
 def sw_steer_action(sender):
 	global_activity.start()
 	try:
-		speed = sender.superview['slide_speed'].value
+		# speed = sender.superview['slide_speed'].value
+		speed = 6
+		car.speed = speed
 		log_item(sender.superview, 'speed/set-absolute?value={}'.format(speed))
-		r = car.exec_function('speed/set-absolute', speed=str(speed))
+		r = car.exec_function('speed/set-absolute', value=str(speed))
 		log_item(sender.superview, r)
 	except Exception as e:
 		console.hud_alert(str(e),'error',0.25)
-		global_activity.stop()
+	redraw_status(sender.superview)
+	global_activity.stop()
 
 @ui.in_background
 def sw_stop_action(sender):
 	global_activity.start()
 	try:
 		log_item(sender.superview, 'speed/set-absolute?value=0')
-		r = car.exec_function('speed/set-absolute', speed=0)
+		car.speed = 0
+		r = car.exec_function('speed/set-absolute', value='0')
 		log_item(sender.superview, r)
 	except Exception as e:
 		console.hud_alert(str(e),'error',0.25)
-		global_activity.stop()
+	redraw_status(sender.superview)
+	global_activity.stop()
 
 def sw_clear_log_action(sender):
 	sender.superview['txt_log'].text = ''
+	
+@ui.in_background
+def sw_update_action(sender):
+	global_activity.start()
+	try:
+		car.reload_status()
+		car.reload_speed()
+	except Exception as e:
+		console.hud_alert(str(e),'error',0.25)
+	redraw_status(sender.superview)
+	global_activity.stop()
 
 v = ui.load_view()
 global_activity = ui.ActivityIndicator()
