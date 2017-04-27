31ec792dfadce2e7874cf3dffe1f2c20fac0b1b7
Streamliner <jimmy.xrail@me.com>
2017-04-27 01:39:14 +0000

    Complete Function Matrix

--- car.py
+++ car.py
@@ -9,18 +9,21 @@ class Car():
 		self.speed = 0
 		self.steps = 0
 		self.obstacle = 0
+		self.state = ''
 	def connect(self,ip_addr='', really=False):
 		if really and (not self.executing):
 			self.session = Session()
 			self.executing = True
-			r = self.session.get('http://'+ip_addr+'/status')
+			r = self.session.get('http://'+ip_addr+'/status', timeout=5)
 			self.executing = False
 			assert r.status_code == 200
+			self.state = ''
 			try:
 				data = loads(r.content)
 				self.steps = data['steps']
 				self.obstacle	= data['obstacle']
-			except: pass
+			except:
+				self.state = 'data'
 			self.connected = True
 			self.ip_addr = 'http://'+ip_addr+'/'
 		else:
@@ -34,19 +37,26 @@ class Car():
 			connection_string += '?' + ','.join([k+'='+v for k,v in kwargs.items()])
 		try:
 			self.executing = True
-			r = self.session.get(connection_string);
+			r = self.session.get(connection_string,timeout=5);
 			self.executing = False
+			self.state = ''
 		except Exception as e:
 			self.executing = False
+			self.state = 'Exception'
 			raise(e)
 		assert r.status_code == 200
 		return loads(r.content)
 
-	def reload_status(self, nc=False):
+	def reload_status(self, nc=True):
 		action = 'status-nc' if nc else 'status'
 		data = self.exec_function(action)
+		if data['status'] == -1:
+			self.state = 'data'
+		else:
+			self.state = ''
 		self.steps = data['steps-traversed']
 		self.obstacle	= data['obstacle']
+		return data
 		
 	def reload_speed(self):
 		data = self.exec_function('speed')
--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,15 @@ def redraw_status(v):
 	else:
 		v['img_tcas_normal'].alpha = 0
 		v['img_tcas_triggered'].alpha = 1
+	if car.state == '':
+		v['lbl_state'].border_color = 'blue'
+		v['lbl_state'].text = 'NOMINAL'
+	elif car.state == 'data':
+		v['lbl_state'].border_color = 'red'
+		v['lbl_state'].text = 'DATA_CORRUPT'
+	else:
+		v['lbl_state'].border_color = 'yellow'
+		v['lbl_state'].text = car.state
 
 @ui.in_background
 def sw_connect_action(sender):
@@ -30,7 +39,9 @@ def sw_connect_action(sender):
 		console.hud_alert('Could not '+('establish ' if sender.value else 'terminate ')+'connection: '+str(e),'error',0.25)
 		log_item(sender.superview, e);
 		sender.value = not sender.value
+		car.state = 'Connection Failed'
 	global_activity.stop()
+	redraw_status(sender.superview)
 
 
 @ui.in_background
@@ -41,24 +52,26 @@ def sw_turn_action(sender):
 		r = car.exec_function('heading/set-relative', value=sender.angle)
 		log_item(sender.superview, r)
 	except Exception as e:
-		console.hud_alert(str(e),'error',0.25)
-	redraw_status(sender.superview)
+		console.hud_alert(e.__repr__(),'error',0.25)
+		car.state = 'Exception'
 	global_activity.stop()
+	redraw_status(sender.superview)
 
 @ui.in_background
 def sw_steer_action(sender):
 	global_activity.start()
 	try:
 		# speed = sender.superview['slide_speed'].value
-		speed = 6
+		speed = int(sender.superview['lbl_speed_f'].text)
 		car.speed = speed
 		log_item(sender.superview, 'speed/set-absolute?value={}'.format(speed))
 		r = car.exec_function('speed/set-absolute', value=str(speed))
 		log_item(sender.superview, r)
 	except Exception as e:
-		console.hud_alert(str(e),'error',0.25)
-	redraw_status(sender.superview)
+		console.hud_alert(e.__repr__()+': '+e.__str__(),'error',0.25)
+		car.state = 'Exception'
 	global_activity.stop()
+	redraw_status(sender.superview)
 
 @ui.in_background
 def sw_stop_action(sender):
@@ -69,9 +82,10 @@ def sw_stop_action(sender):
 		r = car.exec_function('speed/set-absolute', value='0')
 		log_item(sender.superview, r)
 	except Exception as e:
-		console.hud_alert(str(e),'error',0.25)
-	redraw_status(sender.superview)
+		console.hud_alert(e.__repr__(),'error',0.25)
+		car.state = 'Exception'
 	global_activity.stop()
+	redraw_status(sender.superview)
 
 def sw_clear_log_action(sender):
 	sender.superview['txt_log'].text = ''
@@ -80,12 +94,34 @@ def sw_clear_log_action(sender):
 def sw_update_action(sender):
 	global_activity.start()
 	try:
-		car.reload_status()
+		log_item(sender.superview, '/status'+('-nc' if sender.nc else ''))
+		r = car.reload_status(nc=sender.nc)
+		log_item(sender.superview, r)
+		redraw_status(sender.superview)
 		car.reload_speed()
 	except Exception as e:
-		console.hud_alert(str(e),'error',0.25)
+		console.hud_alert(e.__repr__(),'error',0.25)
+		car.state = 'Exception'
 	redraw_status(sender.superview)
 	global_activity.stop()
+	
+@ui.in_background
+def sl_speed_action(sender):
+	v = int(sender.value * 10)
+	sender.superview['lbl_speed_f'].text = str([-8,-7,-6,-5,-4,0,4,5,6,7,8][v])
+	
+@ui.in_background
+def sw_general_action(sender):
+	global_activity.start()
+	try:
+		log_item(sender.superview, sender.endpoint)
+		r = car.exec_function(sender.endpoint)
+		log_item(sender.superview, r)
+	except Exception as e:
+		log_item(sender.superview, e)
+		car.state = 'Exception'
+	global_activity.stop()
+	redraw_status(sender.superview)
 
 v = ui.load_view()
 global_activity = ui.ActivityIndicator()
